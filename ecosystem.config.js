bafa3186e862271a8b426e2a92dac4ffe93fd7c2
Hoang Binh An <anhb@cs-soft.jp>
2023-05-21 11:47:28 +0700

    update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,11 @@ module.exports = {
   apps: [
     {
       name: "NextAppName",
-      exec_mode: "cluster",
-      instances: "2", // Or a number of instances
       script: "npm",
       args: "start",
-      env_local: {
-        APP_ENV: "local", // APP_ENV=local
-      },
-      env_development: {
-        APP_ENV: "dev", // APP_ENV=dev
-      },
-      env_production: {
-        APP_ENV: "prod", // APP_ENV=prod
+      watch: ".",
+      env: {
+        NODE_ENV: "production",
       },
     },
   ],
